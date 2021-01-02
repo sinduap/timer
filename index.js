788e825f03a0306e605992f5a0182bdcef123dfb
Sindu Andita <sinduandita@gmail.com>
2021-01-02 23:27:54 +0700

    a timer app using vanilla javascript

---
+++ index.js
@@ -0,0 +1,23 @@
+const durationInput = document.getElementById('duration');
+const startButton = document.getElementById('start');
+const pauseButton = document.getElementById('pause');
+const circle = document.querySelector('circle');
+
+const perimeter = circle.getAttribute('r') * 2 * Math.PI;
+circle.setAttribute('stroke-dasharray', perimeter);
+
+let duration;
+const timer = new Timer(durationInput, startButton, pauseButton, {
+  onStart(totalDuration) {
+    duration = totalDuration;
+  },
+  onTick(timeRemaining) {
+    circle.setAttribute(
+      'stroke-dashoffset',
+      (perimeter * timeRemaining) / duration - perimeter
+    );
+  },
+  onComplete() {
+    console.log('Timer completed');
+  },
+});
